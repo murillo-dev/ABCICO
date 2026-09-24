416480a673fcca7e3ff9f57cba606946203a384c
Israel Murillo <iemf.dev@gmail.com>
2026-09-24 04:23:36 -0400

    update the docker-entrupoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-mkdir -p /var/www/html/storage/framework/{sessions,views,cache}
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/framework/cache/data
+mkdir -p /var/www/html/storage/logs
+mkdir -p /var/www/html/storage/app/public
+
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 echo "Caching config..."
@@ -10,7 +15,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+# Quita las migraciones de aquí — en el plan gratis la DB "duerme"
+# y puede no estar lista. Las corres manual desde el Shell de Render:
+# php artisan migrate --force
 
 exec "$@"
